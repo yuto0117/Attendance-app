2503a0344fc41efcce685b562add428aa723f7d1
yuto0117 <rnmz79084@gmail.com>
2025-03-11 12:24:08 +0900

    dockerfile作成

--- public/index.php
+++ public/index.php
@@ -13,5 +13,7 @@ if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php'))
 require __DIR__.'/../vendor/autoload.php';
 
 // Bootstrap Laravel and handle the request...
+dd((require_once __DIR__.'/../bootstrap/app.php')
+->handleRequest(Request::capture()));
 (require_once __DIR__.'/../bootstrap/app.php')
     ->handleRequest(Request::capture());

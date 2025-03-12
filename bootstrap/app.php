e34f6b219d0a8c02bbd8614619f748d042ef883f
yuto0117 <rnmz79084@gmail.com>
2025-03-12 11:13:06 +0900

    dockerfile作成

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,7 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Support\Facades\DB;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -19,4 +20,5 @@ return Application::configure(basePath: dirname(__DIR__))
     })
     ->withExceptions(function (Exceptions $exceptions) {
         // dd(dirname(__DIR__),$exceptions);
+        dd(DB::connection()->getPdo(),$exceptions);
     })->create();

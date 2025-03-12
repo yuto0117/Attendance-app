3bc1b799d45d204a1ee4774ea59a0a984d14a494
yuto0117 <rnmz79084@gmail.com>
2025-03-12 11:01:58 +0900

    dockerfile作成

--- public/index.php
+++ public/index.php
@@ -3,7 +3,7 @@
 use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
-
+dd(DB::connection()->getPdo());
 // Determine if the application is in maintenance mode...
 if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
     require $maintenance;

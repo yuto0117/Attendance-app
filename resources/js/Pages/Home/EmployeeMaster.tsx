2571c7675ed5f1b4b2269f95e7e851b146da18be
yuto0117 <rnmz79084@gmail.com>
2025-03-13 12:11:55 +0900

    dockerfile作成

--- resources/js/Pages/Home/EmployeeMaster.tsx
+++ resources/js/Pages/Home/EmployeeMaster.tsx
@@ -37,7 +37,7 @@ const EmployeeMaster: React.FC<{ auth: Auth }> = ({ auth }) => {
 
     const [employees, setEmployees] = useState<Employee[]>([]);
     const apiUrl = process.env.REACT_APP_API_URL;
-
+console.log(apiUrl);
     useEffect(() => {
         axios.get(`${apiUrl}/api/employees`)
             .then(response => {
